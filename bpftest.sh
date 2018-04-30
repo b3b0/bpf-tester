922c5000496c368893c8ae68c57c9f108ff709ec
b3b0 <b3b0@users.noreply.github.com>
2018-04-30 13:02:40 -0500

    Update bpftest.sh

--- bpftest.sh
+++ bpftest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CURRENTFILTER=$(cat /path/to/your/bpf.conf)
-timeout 15s tshark -n -f "${CURRENTFILTER}" -i em2 -T fields -e ip.src -e ip.dst > bpftest.txt
+timeout 15s tshark -n -f "${CURRENTFILTER}" -i eth0 -T fields -e ip.src -e ip.dst > bpftest.txt #change eth0 to your promisc interface
 cat bpftest.txt | grep 8.8.8.8 || echo "No results for 8.8.8.8" # this is optional, but can tell you if individual IPs or ranges are in the outted file.
